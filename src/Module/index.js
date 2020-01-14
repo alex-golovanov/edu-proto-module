48ec7ced355648fabea52235bd533ff8e497a2c7
Alexander Golovanov <alex.golovanov@gmail.com>
2020-01-14 18:42:15 +0300

    Added module routing

--- src/Module/index.js
+++ src/Module/index.js
@@ -1,23 +1,68 @@
 import React from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ChatIcon from '@material-ui/icons/Chat';
+import SearchIcon from '@material-ui/icons/Search';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+
 import Button from '../shared/Button/';
 import IconButton from '../shared/IconButton/';
-import MenuIcon from '@material-ui/icons/Menu';
 
 import styles from './styles.scss';
 
+const Start = () => {
+  return (
+    <>
+      <Button variant="contained" color="primary" component={NavLink} to="/module/text-task">Text task</Button>
+      <Button variant="contained" color="primary" component={NavLink} to="/module/video-task">Video task</Button>
+    </>
+  )
+}
+
+const TextTask = () => {
+  return (
+    <>
+      <div>TextTask block</div>
+      <Button variant="contained" color="primary" component={NavLink} to="/module">To start</Button>
+    </>
+  )
+}
+
+const VideoTask = () => {
+  return (
+    <>
+      <div>VideoTask popup</div>
+      <Button variant="contained" color="primary" component={NavLink} to="/module">To start</Button>
+    </>
+  )
+}
 
 export default function Module() {
 
     return (
         <div className="root">
             <div className="viewport">
-                <div class="header">
+                <div className="header">
                   <IconButton><MenuIcon /></IconButton>
-                  <Button>abc</Button>
+                  <Button>История</Button>
+                  <Button>Пропустить</Button>
                 </div>
                 <div className="sidenav">
-                  <Button>abc</Button>
+                  <IconButton><EmojiEmotionsIcon /></IconButton>
+                  <IconButton><NotificationsIcon /></IconButton>
+                  <IconButton><ChatIcon /></IconButton>
+                  <IconButton><SearchIcon /></IconButton>
                 </div>
+                <div className="content">
+                  <Switch>
+                    <Route exact path="/module" component={Start} />
+                    <Route path="/module/text-task" component={TextTask} />
+                    <Route path="/module/video-task" component={VideoTask} />
+                  </Switch>
+                </div>
+
             </div>
         </div>
     );

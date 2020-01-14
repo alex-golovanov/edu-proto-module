95b0564ee17280e10564d6c6af03664a3713116c
Alex <alex.golovanov@gmail.com>
2020-01-14 12:08:49 +0300

    Testing github sync

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Home from "./Home";
 const About = () => <div>About</div>;
-const Topics = () => <div>Topics</div>;
 
 const App = () => (
   <Router>
@@ -16,16 +15,12 @@ const App = () => (
         <li>
           <Link to="/about">About</Link>
         </li>
-        <li>
-          <Link to="/topics">Test</Link>
-        </li>
       </ul>
 
       <hr />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
       </Switch>
     </div>
   </Router>

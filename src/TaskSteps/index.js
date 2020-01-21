ec37d5ca4a27a5b527df643751429549da62b58f
Ivan Baranov <293344asd@gmail.com>
2020-01-21 11:52:11 +0700

    added TaskHeader component

--- src/TaskSteps/index.js
+++ src/TaskSteps/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import './styles.scss'
+import TaskHeader from './TaskHeader'
 
 export default function TaskSteps() {
   return (
     <div>
+      <TaskHeader />
       <h1>Hello</h1>
     </div>
   )

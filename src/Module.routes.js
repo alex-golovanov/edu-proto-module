fb677746723fced48df2fa9a20b5051cc6254d71
Ivan Baranov <293344asd@gmail.com>
2020-01-21 09:25:24 +0700

    separated module paths

---
+++ src/Module.routes.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import Module from './Module'
+import ModuleOverview from './ModuleOverview'
+import TaskVideo from './TaskVideo'
+import TaskText from './TaskText'
+import ModuleElementLevelTwo from './ModuleElement/ModuleElementLevelTwo'
+import ModuleElement from './ModuleElement'
+import Reflection from './Reflection'
+import SlidesExample from './SlidesExample'
+
+export default function ModuleRoutes() {
+  return (
+    <Switch>
+      <Route exact path="/module" component={Module} />
+      <Route path="/module/overview" component={ModuleOverview} />
+      <Route path="/module/task/video" component={TaskVideo} />
+      <Route path="/module/task/text" component={TaskText} />
+      <Route exact path="/module/element/two" component={ModuleElementLevelTwo} />
+      <Route exact path="/module/element/two-done" component={ModuleElementLevelTwo} />
+      <Route path="/module/element/:level?" component={ModuleElement} />
+      <Route path="/module/reflection" component={Reflection} />
+      <Route path="/slides/:taskId?/:meta?" component={SlidesExample} />
+      <Redirect to="/module" />
+    </Switch>
+  )
+}

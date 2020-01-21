42354f42d187a232a2f8ab67eb459191c74cca66
Ivan Baranov <293344asd@gmail.com>
2020-01-21 15:35:14 +0700

    fix back button link

--- src/TaskSteps/TaskHeader/index.js
+++ src/TaskSteps/TaskHeader/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import './styles.scss'
 import IconButton from '@material-ui/core/IconButton'
 import { makeStyles } from '@material-ui/core'
-import Close from '@material-ui/icons/Close'
+import ArrowBack from '@material-ui/icons/ArrowBack'
 import ChatIcon from '@material-ui/icons/Chat'
+import { NavLink } from 'react-router-dom'
+
+import './styles.scss'
 
 const useStyles = makeStyles({
   backBtn: {
@@ -13,6 +15,9 @@ const useStyles = makeStyles({
     htmlColor: '#000000',
     backgroundColor: '#FFF',
     justifySelf: 'flex-start'
+  },
+  backIcon: {
+    color: '#212122'
   },
   chatIcon: {
     color: 'rgba(0, 0, 0, 0.54)'
@@ -26,10 +31,19 @@ export default function TaskHeader({ step }) {
     <div className="task-header">
       <span className="sub-header">История • Эпоха Возрождения • 2.0</span>
       <div className="task-header__main">
+<<<<<<< HEAD
         <IconButton disableRipple className={classes.backBtn}>
           <Close />
         </IconButton>
         <h1 className="task-header__step">{step === '1' ? 'I' : 'II'} шаг</h1>
+=======
+        <NavLink to="/module/element/two">
+          <IconButton disableRipple className={classes.backBtn}>
+            <ArrowBack className={classes.backIcon} />
+          </IconButton>
+        </NavLink>
+        <h1 className="task-header__step">I шаг</h1>
+>>>>>>> fix back button link
         <div className="task-header__chat">
           <ChatIcon className={classes.chatIcon} />
           <span className="task-header__chat-number">12</span>
